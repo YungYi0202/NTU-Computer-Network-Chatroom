1aaace98258ff80dff3762656b503e906d2acc96
YungYi0202 <cathy@anyday.com.tw>
2021-12-28 15:51:21 +0800

    Modify Server

--- client.cpp
+++ client.cpp
@@ -15,129 +15,187 @@
 
 #include <algorithm>
 #include <string>
+#include <sstream>
 #define ERR_EXIT(a) \
   do {              \
     perror(a);      \
     exit(1);        \
   } while (0)
 #define BUF_LEN 1024
+#define MAXFD 1024
+#define SVR_PORT 8080
 
 #define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 #define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
-class Server {
- public:  // TODO: modify access
-  static const int MAX_FD = 1024;
+#define STATE_UNUSED 0
+#define STATE_NO_USERNAME 1
+#define STATE_LINK 2
 
-  std::string response =
-      "HTTP/1.1 200 OK\r\n"
-      "Content-Type: text/html; charset=UTF-8\r\n\r\n"
-      "<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
+class Server {
+public:  // TODO: modify access
+  class client {
+  public:
+    int fd;
+    int state;
+    char username[10];
+    client() {
+      reset();
+    }
+    void reset() {
+      fd = -1;
+      state = STATE_UNUSED;
+      bzero(username, sizeof(username));
+    }
+    void start(int _fd) {
+      fd = _fd;
+      state = STATE_NO_USERNAME;
+    }
+  };
+
+  /*
+  class Http {
+  public:
+    std::string get() {
+      return getHeader() + getContent();
+    }
+    std::string getHeader() {
+      return "HTTP/1.1 200 OK\r\n"
+      "Content-Type: text/html; charset=UTF-8\r\n\r\n";
+    }
+    std::string getContent() {
+      return "<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
       "<style>body { background-color: #111 }"
       "h1 { font-size:4cm; text-align: center; color: black;"
       " text-shadow: 0 0 2mm red}</style></head>"
       "<body><h1>Goodbye, world!</h1></body></html>\r\n";
-
-  struct client {
-    int fd;  // fd to talk with client
-    char username[10];
-
-    client() {
-      fd = -1;
-      bzero(username, sizeof(username));
     }
   };
-  client clients[MAX_FD];
+  */
 
-  fd_set master_RDset, working_RDset;
+  static const int maxfd = MAXFD;
+  int sockfd;
+  std::string indexResponse =
+      "HTTP/1.1 200 OK\r\n"
+      "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+      "<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title></head>"
+      "<body><h1>Goodbye, world!</h1><form method=\"post\" action=\"\">"
+      "<input type=\"text\" name=\"username\" />"
+      "<input type=\"submit\" />"
+      "</form></body></html>\r\n";
+
+  client clients[maxfd];
+  fd_set master_rfds, working_rfds;
   char buf[BUF_LEN];
 
   void initServer() {
     int one = 1;
     struct sockaddr_in svr_addr;
 
-    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_fd < 0) err(1, "can't open socket");
+    this->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) err(1, "can't open socket");
 
     // TODO: Check out what this is for.
-    setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
 
-    int port = 8080;
     svr_addr.sin_family = AF_INET;
     svr_addr.sin_addr.s_addr = INADDR_ANY;
-    svr_addr.sin_port = htons(port);
+    svr_addr.sin_port = htons(SVR_PORT);
 
-    if (bind(sock_fd, (struct sockaddr *)&svr_addr, sizeof(svr_addr)) == -1) {
-      close(sock_fd);
+    if (bind(sockfd, (struct sockaddr *)&svr_addr, sizeof(svr_addr)) == -1) {
+      close(sockfd);
       err(1, "Can't bind");
     }
 
-    listen(sock_fd, MAX_FD);
-    serve(sock_fd);
+    listen(sockfd, maxfd);
   }
-  void serve(int sock_fd) {
-    clients[sock_fd].fd = sock_fd;
 
-    FD_ZERO(&master_RDset);
-    FD_SET(sock_fd, &master_RDset);
+  void serve() {
+    clients[sockfd].fd = sockfd;
+
+    FD_ZERO(&master_rfds);
+    FD_SET(sockfd, &master_rfds);
 
     struct sockaddr_in client_addr;
-    int cli_fd;
+    int clifd;
     int clilen = sizeof(client_addr);
 
-    client *clientP;  // Pointer to a specific client.
-
     while (1) {
-      memcpy(&working_RDset, &master_RDset, sizeof(master_RDset));
-      select(MAX_FD, &working_RDset, NULL, NULL, NULL);
-      if (FD_ISSET(sock_fd, &working_RDset)) {
-        if ((cli_fd = accept(sock_fd, (struct sockaddr *)&client_addr,
+      memcpy(&working_rfds, &master_rfds, sizeof(master_rfds));
+      select(maxfd, &working_rfds, NULL, NULL, NULL);
+      if (FD_ISSET(sockfd, &working_rfds)) {
+        if ((clifd = accept(sockfd, (struct sockaddr *)&client_addr,
                              (socklen_t *)&clilen)) < 0) {
           ERR_EXIT("accept");
           continue;
         }
-        FD_SET(cli_fd, &master_RDset);
-        fprintf(stderr, "Accept! client fd: %d\n", cli_fd);
-        clientP = &clients[cli_fd];
-        clientP->fd = cli_fd;
-        handle_write(cli_fd, response, true);
+        FD_SET(clifd, &master_rfds);
+        fprintf(stderr, "=============================\n");
+        fprintf(stderr, "Accept! client fd: %d\n", clifd);
+        fprintf(stderr, "=============================\n");
+        clients[clifd].start(clifd);
+      }
+
+      for (int fd = sockfd + 1; fd < maxfd; fd++) {
+        if (!FD_ISSET(fd, &working_rfds)) continue;
+        handleRead(fd);
+        std::stringstream ss;
+        std::string req(buf);
+        ss << req;
+        std::string command, target;
+        ss >> command >> target;
+        
+        if (command == "GET") {
+          if (target == "/") {
+            handleWrite(fd, indexResponse, true);
+          } else {
+            // TODO:
+            fprintf(stderr, "========%s %s=========\n",command.c_str(), target.c_str());
+          }
+        } else if (command == "POST") {
+
+        }
+        else {
+          fprintf(stderr, "========%s %s=========\n",command.c_str(), target.c_str());
+        }
+        closeFD(fd);
       }
     }
   }
-  static void init_client(client *cli) {
-    cli->fd = -1;
-    bzero(cli->username, sizeof(cli->username));
+
+  void closeFD(int fd) {
+    fprintf(stderr, "closeFD: %d\n", fd);
+    FD_CLR(fd, &master_rfds);   //?
+    FD_CLR(fd, &working_rfds);  //?
+    close(fd);
+    clients[fd].reset();
   }
 
-  static void free_client(client *cli) { init_client(cli); }
-
-  void closeFD(client *cli) {
-    fprintf(stderr, "closeFD: %d\n", cli->fd);
-    FD_CLR(cli->fd, &master_RDset);   //?
-    FD_CLR(cli->fd, &working_RDset);  //?
-    close(cli->fd);
-    free_client(cli);
-  }
-
-  int handle_read(int fd) {
+  int handleRead(int fd) {
     bzero(buf, BUF_LEN);
     int ret = read(fd, buf, sizeof(buf));
     if (ret <= 0 && fd > 0 && clients[fd].fd != -1) {
-      fprintf(stderr, "handle_read: fd:%d closeFD\n", fd);
-      closeFD(&clients[fd]);
+      fprintf(stderr, "handleRead: fd:%d closeFD\n", fd);
+      closeFD(fd);
+    } else {
+      fprintf(stderr, "========handleRead: fd:%d=========\n",fd);
+      fprintf(stderr, "%s\n", buf);
+      fprintf(stderr, "=============================\n");
     }
     return ret;
   }
 
-  int handle_write(int fd, std::string str, bool isHTTP) {
+  int handleWrite(int fd, std::string str, bool isHTTP) {
     sprintf(buf, "%s", str.c_str());
     int writeLen = (isHTTP) ? strlen(buf) - 1 : strlen(buf);
     int ret = write(fd, buf, writeLen);
     if (ret != writeLen && fd > 0 && clients[fd].fd != -1) {
-      fprintf(stderr, "handle_write: fd:%d closeFD\n", fd);
-      closeFD(&clients[fd]);
+      fprintf(stderr, "handleWrite: fd:%d closeFD\n", fd);
+      closeFD(fd);
     } else {
-      fprintf(stderr, "handle_write: fd:%d str:%s\n", fd, str.c_str());
+      fprintf(stderr, "========handleWrite: fd:%d=========\n",fd);
+      fprintf(stderr, "%s\n", buf);
+      fprintf(stderr, "=============================\n");
     }
     return ret;
   }
@@ -301,7 +359,8 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  // server.initServer();
+  server.initServer();
+  server.serve();
 
   char ip[32], port[32];
   char *pch;
@@ -314,5 +373,5 @@ int main(int argc, char *argv[]) {
   int port_l = strlen(port);
   for (int i = 0; i < port_l; i++) port_num = port_num * 10 + port[i] - '0';
 
-  client.initClient(ip, port_num);
+  // client.initClient(ip, port_num);
 }
